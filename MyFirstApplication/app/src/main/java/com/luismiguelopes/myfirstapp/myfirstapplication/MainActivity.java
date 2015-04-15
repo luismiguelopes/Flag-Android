5304b6b9f8fdda8a7b15c2432517c24421fe31a4
luismiguelopes <geral@luismiguelopes.com>
2015-04-15 12:21:15 +0100

    V1.1
    - Criação de Log’s
    - Criação Show Button com Toast

--- MyFirstApplication/app/src/main/java/com/luismiguelopes/myfirstapp/myfirstapplication/MainActivity.java
+++ MyFirstApplication/app/src/main/java/com/luismiguelopes/myfirstapp/myfirstapplication/MainActivity.java
@@ -1,20 +1,29 @@
 package com.luismiguelopes.myfirstapp.myfirstapplication;
 
+import android.content.Context;
+import android.os.PersistableBundle;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity implements View.OnClickListener{
 
+
+    private final static String LOG_TAG = "LOG_TAG";
     private TextView txtCounter = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        Log.d(LOG_TAG, "onCreate");
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -26,6 +35,12 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
 
         Button btnDecr = (Button) findViewById(R.id.btnDecr);
         btnDecr.setOnClickListener(this);
+
+        Button show = (Button) findViewById(R.id.show);
+        show.setOnClickListener(this);
+
+
+
 
         /*
         Button btnIncr = (Button) findViewById(R.id.btnIncr);
@@ -67,6 +82,69 @@ public class MainActivity extends ActionBarActivity implements View.OnClickListe
             int current = Integer.parseInt(txtCounter.getText().toString());
             txtCounter.setText((current - 1) + "");
         }
+        if (v.getId() == R.id.show) {
+            Context context = getApplicationContext();
+            CharSequence text = txtCounter.getText();
+            int duration = Toast.LENGTH_SHORT;
+            Toast.makeText(context, text, duration).show();
+        }
 
+    }
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        Log.d(LOG_TAG, "onSaveInstanceState");
+
+        outState.putString("COUNTER_VALUE", txtCounter.getText().toString());
+
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+
+        Log.d(LOG_TAG, "onRestoreInstanceState");
+        txtCounter.setText(savedInstanceState.getString("COUNTER_VALUE"));
+    }
+
+    @Override
+    protected void onStart() {
+
+        Log.d(LOG_TAG,"onStart");
+
+        super.onStart();
+    }
+
+    @Override
+    protected void onResume() {
+
+        Log.d(LOG_TAG,"onResume");
+
+        super.onResume();
+    }
+
+    @Override
+    protected void onStop() {
+
+        Log.d(LOG_TAG,"onStop");
+
+        super.onStop();
+    }
+
+    @Override
+    protected void onRestart() {
+
+        Log.d(LOG_TAG,"onRestart");
+
+        super.onRestart();
+    }
+
+    @Override
+    protected void onDestroy() {
+
+        Log.d(LOG_TAG,"onDestroy");
+
+        super.onDestroy();
     }
 }
